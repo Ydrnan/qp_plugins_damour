a33272d52d1f6127d21114c704a34048b703f5d8
yannd <yann.damour@hotmail.fr>
2021-07-01 15:28:21 +0200

    fix debug_rotation_matrix

--- test_newton/debug_rotation_matrix.irp.f
+++ test_newton/debug_rotation_matrix.irp.f
@@ -11,16 +11,17 @@ program debug_rotation_matrix
 
   double precision, allocatable :: grad(:,:),R(:,:),R1(:,:)
   double precision, allocatable :: H(:,:),H1(:,:),h_f(:,:,:,:)
-  double precision, allocatable :: Hm1(:,:),v_grad(:),m_Hm1g(:,:),Hm1g(:)
-  double precision, allocatable :: prev_mos(:,:),new_mos(:,:),new_mos1(:,:)
+  double precision, allocatable :: v_grad(:),m_x(:,:),x(:),W(:,:),e_val(:)
   integer                       :: info,method
   integer                       :: n
   integer                       :: i,j,p,q,k
-  integer                       :: nb_iter_trust
-  double precision              :: trust_radius,rho,energy,e_model
+  integer                       :: nb_iter
+  double precision              :: delta,rho,energy,e_model
   logical :: cancel_step
   integer :: nb_error
   double precision :: max_error, threshold, max_elem
+  double precision :: prev_energy
+  logical :: converged
   ! grad   : mo_num by mo_num double precision matrix, the gradient for the gradient method
   ! R      : mo_num by mo_num double precision matrix, rotation matrix to change the MOs
   ! H      : n by n double precision matrix, Hessian matrix
@@ -56,27 +57,22 @@ program debug_rotation_matrix
   !============
 
   allocate(v_grad(n),R(mo_num,mo_num),R1(mo_num,mo_num))
-  allocate(H(n,n),H1(n,n),Hm1(n,n),m_Hm1g(mo_num,mo_num),Hm1g(n))
+  allocate(H(n,n),m_x(mo_num,mo_num),x(n))
   allocate(h_f(mo_num,mo_num,mo_num,mo_num))
-  allocate(prev_mos(ao_num,mo_num))
-
+  allocate(W(n,n),e_val(n))
   !=============
   ! Calculation
   !=============
 
-  logical :: converged
-
   converged = .False.
-
-  integer :: nb_iter
-  double precision :: prev_energy
-
-  trust_radius = 0d0
-  prev_energy = 0.d0
+  prev_energy = 0d0
+  rho = 0.5d0
+  delta = 0d0
   cancel_step = .False.
 
-  nb_iter_trust = 0
-  ! Gradient and norm
+  nb_iter = 0
+  
+! Gradient and norm
   call gradient(n,v_grad,max_elem)
   
   ! Hessian and norm
@@ -86,13 +82,15 @@ program debug_rotation_matrix
     call diag_hess(n,H,h_f) !h_f -> debug
   endif
 
+  call diagonalization_hessian(n,H,e_val,w)
+
   ! Inversion of the hessian
-  call trust_region(n,method,H,v_grad,m_Hm1g,prev_energy,nb_iter,trust_radius,e_model,cancel_step,prev_mos)
+  call trust_region(n,method,nb_iter,H,v_grad,rho,e_val,W,x,m_x,delta)
 
   ! Rotation matrix
-  call rotation_matrix(m_Hm1g,mo_num,R,mo_num,mo_num,info)
+  call rotation_matrix(m_x,mo_num,R,mo_num,mo_num,info) 
 
-  call rotation_matrix_omp(m_Hm1g,mo_num,R1,mo_num,mo_num,info) 
+  call rotation_matrix_omp(m_x,mo_num,R1,mo_num,mo_num,info)
 
   R = R1 - R
   
@@ -117,7 +115,9 @@ program debug_rotation_matrix
   print*,'Nb error', nb_error
   print*,'max_error', max_error
 
-  deallocate(v_grad,H,Hm1,m_Hm1g,R,Hm1g)
+  deallocate(v_grad,H,m_x,x,R,R1)
   deallocate(h_f)
+  deallocate(W)
+  deallocate(e_val)
 
 end

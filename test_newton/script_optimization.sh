f7aea787b427b4a71b19142e60b7ae8d4532ac6a
yannd <yann.damour@hotmail.fr>
2021-09-26 21:00:46 +0200

    script for the optimization

--- test_newton/script_optimization.sh
+++ test_newton/script_optimization.sh
@@ -1,30 +1,45 @@
 #!/bin/bash
 #SBATCH -p xeonv3 -N 1 -n 1 -c 24 --exclusive
-source /home/ydamour/qp2/quantum_package.rc
+
+# !!! The path of must be changed !!!
+source /home/ydamour/qp2/quantum_package.rc # here 
+
+# Load modules
 module load intel/2019.0
 module load python/3.7.6-gcc-9.2.0
 module load gcc/8.2.0
 
-# initial ezfio
-INITIAL_DIR=benzene
+# ==========================
+# !!! Starting directory !!!
+# ==========================
 
-# new ezfio for the optimization
-XYZ=benzene
-BASIS=HF_opt_S2
-DIR=${XYZ}_${BASIS}.ezfio
-FILE=${XYZ}_${BASIS}
+START_DIR=benzene.ezfio
+START_FILE=benzene
 
-cp -r ${INITIAL_DIR} ${DIR}
+# If the Starting directory contains the starting orbitals
+# (HF, natural, localized, ...)
+# comment the three following lines
 
-qp set_file $DIR
-qp reset -a
-qp reset -d
+qp set_file ${START_DIR}
+qp run scf > ${START_DIR}/${START_FILE}.scf.out
 
-# SCF
-qp run scf > ${DIR}/${FILE}.scf
+# Frozen core ?
+qp set_frozen_core >  ${START_DIR}/${START_FILE}.fc.out
 
-# frozen core ?
-qp set_frozen_core > ${DIR}/${FILE}.frozen
+# ======================================
+# !!! Directory for the optimization !!!
+# ======================================
+
+XYZ=benzene  # Name
+BASIS=cc_pvdz_opt # Basis + options
+DIR=${XYZ}_${BASIS}.ezfio # Directory
+FILE=${XYZ}_${BASIS} # Base for the filenames
+
+# Copy the .ezfio in order to keep the starting OMs somewhere
+cp -r ${START_DIR} ${DIR}
+
+qp set_file ${DIR} # set the file
+qp reset -d        # delete the determinants
 
 # Some changes
 qp set determinants read_wf true
@@ -36,69 +51,107 @@ qp set ao_two_e_erf_ints io_ao_two_e_integrals_erf write
 qp set determinants s2_eig true
 #qp set determinants s2_eig false
 
-# Starting number of determinants
+# Starting number of determinants for the optimization
 Ndet=5
 
+# Some comments in temporary files
+printf "%10s\n" N_det > ${DIR}/${FILE}_n_det.dat
+printf "%15s\n" E_var > ${DIR}/${FILE}_energy.dat
+printf "%15s %15s\n" E_PT2 Error_PT2 > ${DIR}/${FILE}_pt2.dat
+printf "%15s %15s\n" E_rPT2 Error_rPT2 > ${DIR}/${FILE}_rpt2.dat
+printf "%15s\n" E+PT2 > ${DIR}/${FILE}_e_p_pt2.dat
+printf "%15s\n" E+rPT2 > ${DIR}/${FILE}_e_p_rpt2.dat
+
+# =================================
+# !!! Loop for the optimization !!!
+# =================================
+
+# Loops while the number of determinants in smaller than max number
 while [ ${Ndet} -lt 200000 ]
 do
-    echo ${Ndet}
-    qp set determinants n_det_max ${Ndet}
-    qp run fci > ${DIR}/${FILE}_${Ndet}.fci
+    qp set determinants n_det_max ${Ndet} # set the new number of determinants for the CIPSI
+    qp run fci > ${DIR}/${FILE}_${Ndet}.fci.out # CISPI calculation
+
+	# Some informations from the CISPI
+    grep "Summary at N_det = " ${DIR}/${FILE}_${Ndet}.fci.out | tail -1 | awk '{printf "%10s\n", $5}' >> ${DIR}/${FILE}_n_det.dat
+    grep "# E   " ${DIR}/${FILE}_${Ndet}.fci.out | tail -1 | awk '{printf "%15s\n", $3}' >> ${DIR}/${FILE}_energy.dat
+    grep "# PT2   " ${DIR}/${FILE}_${Ndet}.fci.out | tail -1 | awk '{printf "%15s %15s\n", $3, $4}' >> ${DIR}/${FILE}_pt2.dat
+    grep "# rPT2   " ${DIR}/${FILE}_${Ndet}.fci.out | tail -1 | awk '{printf "%15s %15s\n", $3, $4}' >> ${DIR}/${FILE}_rpt2.dat
+	grep "# E+PT2   " ${DIR}/${FILE}_${Ndet}.fci.out | tail -1 | awk '{printf "%15s\n", $3}' >> ${DIR}/${FILE}_e_p_pt2.dat
+    grep "# E+rPT2   " ${DIR}/${FILE}_${Ndet}.fci.out | tail -1 | awk '{printf "%15s\n", $3}' >> ${DIR}/${FILE}_e_p_rpt2.dat
  
-    grep "Summary at N_det = " ${DIR}/${FILE}_${Ndet}.fci | tail -1 >> ${DIR}/${FILE}_n_det.dat
-    grep "# E   " ${DIR}/${FILE}_${Ndet}.fci | tail -1 >> ${DIR}/${FILE}_energy.dat
-    grep "# PT2   " ${DIR}/${FILE}_${Ndet}.fci | tail -1 >> ${DIR}/${FILE}_pt2.dat
-    grep "# rPT2   " ${DIR}/${FILE}_${Ndet}.fci | tail -1 >> ${DIR}/${FILE}_rpt2.dat
+    qp run orb_opt_trust > ${DIR}/${FILE}_opt_trash_${Ndet}.dat # Orbital optimization
 
-    qp run org_orb_opt_trust > ${DIR}/${FILE}_opt_trash_${Ndet}.dat
-    grep "Max element in gradient :" ${DIR}/${FILE}_opt_trash_${Ndet}.dat > ${DIR}/${FILE}_grad_${Ndet}.dat
-    grep "Energy of state    1" ${DIR}/${FILE}_opt_trash_${Ndet}.dat > ${DIR}/${FILE}_opt_energy_${Ndet}.dat
+    # Evolution of the energy during the optimization
+    echo "Evolution of the energy during the orbital optimization for" ${Ndet} "determinants" > ${DIR}/${FILE}_opt_energy_${Ndet}.dat
+    grep "Energy of state    1" ${DIR}/${FILE}_opt_trash_${Ndet}.dat | awk '{print $6}' >> ${DIR}/${FILE}_opt_energy_${Ndet}.dat
 
-    qp run pt2 > ${DIR}/${FILE}_${Ndet}.pt2 
+    qp run pt2 > ${DIR}/${FILE}_${Ndet}.pt2 # Calculation after the optimization
 
-    grep "Summary at N_det = " ${DIR}/${FILE}_${Ndet}.pt2 | tail -1 >> ${DIR}/${FILE}_n_det.dat
-    grep "# E   " ${DIR}/${FILE}_${Ndet}.pt2 | tail -1 >> ${DIR}/${FILE}_energy.dat
-    grep "# PT2   " ${DIR}/${FILE}_${Ndet}.pt2 | tail -1 >> ${DIR}/${FILE}_pt2.dat
-    grep "# rPT2   " ${DIR}/${FILE}_${Ndet}.pt2 | tail -1 >> ${DIR}/${FILE}_rpt2.dat
+	# Some informations from the new calculations
+    grep "Summary at N_det = " ${DIR}/${FILE}_${Ndet}.pt2.out | tail -1 | awk '{printf "%10s\n", $5}' >> ${DIR}/${FILE}_n_det.dat
+    grep "# E   " ${DIR}/${FILE}_${Ndet}.pt2.out | tail -1 | awk '{printf "%15s\n", $3}' >> ${DIR}/${FILE}_energy.dat
+    grep "# PT2   " ${DIR}/${FILE}_${Ndet}.pt2.out | tail -1 | awk '{printf "%15s %15s\n", $3, $4}' >> ${DIR}/${FILE}_pt2.dat
+    grep "# rPT2   " ${DIR}/${FILE}_${Ndet}.pt2.out | tail -1 | awk '{printf "%15s %15s\n", $3, $4}' >> ${DIR}/${FILE}_rpt2.dat
+    grep "# E+PT2   " ${DIR}/${FILE}_${Ndet}.fci.out | tail -1 | awk '{printf "%15s\n", $3}' >> ${DIR}/${FILE}_e_p_pt2.dat
+    grep "# E+rPT2   " ${DIR}/${FILE}_${Ndet}.fci.out | tail -1 | awk '{printf "%15s\n", $3}' >> ${DIR}/${FILE}_e_p_rpt2.dat
 
-Ndet=$[${Ndet}*2]    
+Ndet=$[${Ndet}*2] # Ndet = Ndet * 2
 done
 
-paste ${DIR}/${FILE}_n_det_opt.dat ${DIR}/${FILE}_energy_opt.dat > ${DIR}/${FILE}_result_opt.dat
+# File containing the results
+# It looks like:
+# N_det ...
+# 5     ... -> energies before the optimization for 5 determinants
+# 5     ... -> energies after the optimization for 5 determinants
+# 10    ... -> energies before the optimization for 10 determinants
+# 10    ... -> energies after the optimization for 10 determinants
+# ...   ...
+
 paste ${DIR}/${FILE}_n_det.dat ${DIR}/${FILE}_energy.dat > ${DIR}/${FILE}_tmp1.dat
 paste ${DIR}/${FILE}_tmp1.dat ${DIR}/${FILE}_pt2.dat > ${DIR}/${FILE}_tmp2.dat
-paste ${DIR}/${FILE}_tmp2.dat ${DIR}/${FILE}_rpt2.dat > ${DIR}/${FILE}_result.dat
+paste ${DIR}/${FILE}_tmp2.dat ${DIR}/${FILE}_rpt2.dat > ${DIR}/${FILE}_tmp3.dat
+paste ${DIR}/${FILE}_tmp3.dat ${DIR}/${FILE}_e_p_pt2.dat > ${DIR}/${FILE}_tmp4.dat
+paste ${DIR}/${FILE}_tmp4.dat ${DIR}/${FILE}_e_p_rpt2.dat > ${DIR}/${FILE}_result_opt.dat
 
-# CIPSI calculation with the optimized orbitals
-DIR2=${FILE}_cipsi.ezfio
-FILE2=${FILE}_cipsi
-cp -r ${DIR} ${DIR2}
+# ================================================
+# !!! CIPSI calculation with the optimized MOs !!!
+# ================================================
+
+DIR2=${FILE}_cipsi.ezfio # Dir
+FILE2=${FILE}_cipsi # Base for the filenames
+
+# New directory for the CISPI
+cp -r ${DIR} ${DIR2} 
 
 qp set_file ${DIR2}
-qp reset -d
-qp set determinants read_wf true
-qp set determinants mo_label MCSCF
-qp set mo_basis mo_label MCSCF
+qp reset -d # delete the determinants in order to do a clean CIPSI
 
-# S^2 true or false
-#qp set determinants s2_eig true
-qp set determinants s2_eig false
+qp set determinants n_det_max 2e6 # max number of determinants
+qp run fci > ${DIR2}/${FILE2}.fci.out # CIPSI
 
-# Number of determinants for the CIPSI
-qp set determinants n_det_max 3e6
+# Some comments in temporary files
+printf "%10s\n" N_det > ${DIR2}/${FILE2}_n_det.dat
+printf "%15s\n" E_var > ${DIR2}/${FILE2}_energy.dat
+printf "%15s %15s\n" E_PT2 Error_PT2 > ${DIR2}/${FILE2}_pt2.dat
+printf "%15s %15s\n" E_rPT2 Error_rPT2 > ${DIR2}/${FILE2}_rpt2.dat
+printf "%15s\n" E+PT2 > ${DIR2}/${FILE2}_e_p_pt2.dat
+printf "%15s\n" E+rPT2 > ${DIR2}/${FILE2}_e_p_rpt2.dat
 
-qp run fci > ${DIR2}/${FILE2}.fci
+# Some informations from the CIPSI
+grep "Summary at N_det = " ${DIR2}/${FILE2}.fci.out | awk '{printf "%10s\n", $5}' >> ${DIR2}/${FILE2}_n_det.dat
+grep "# E   " ${DIR2}/${FILE2}.fci.out | awk '{printf "%15s\n", $3}' >> ${DIR2}/${FILE2}_energy.dat
+grep "# PT2   " ${DIR2}/${FILE2}.fci.out | awk '{printf "%15s %15s\n", $3, $4}' >> ${DIR2}/${FILE2}_pt2.dat
+grep "# rPT2   " ${DIR2}/${FILE2}.fci.out | awk '{printf "%15s %15s\n", $3, $4}' >> ${DIR2}/${FILE2}_rpt2.dat
+grep "# E+PT2   " ${DIR2}/${FILE}_${Ndet}.fci | awk '{printf "%15s\n", $3}' >> ${DIR2}/${FILE2}_e_p_pt2.dat
+grep "# E+rPT2   " ${DIR2}/${FILE}_${Ndet}.fci | awk '{printf "%15s\n", $3}' >> ${DIR2}/${FILE2}_e_p_rpt2.dat
 
-grep "Summary at N_det = " ${DIR2}/${FILE2}.fci >> ${DIR2}/${FILE2}_n_det.dat
-grep "# E   " ${DIR2}/${FILE2}.fci >> ${DIR2}/${FILE2}_energy.dat
-grep "# PT2   " ${DIR2}/${FILE2}.fci >> ${DIR2}/${FILE2}_pt2.dat
-grep "# rPT2   " ${DIR2}/${FILE2}.fci >> ${DIR2}/${FILE2}_rpt2.dat
-
-paste ${DIR2}/${FILE2}_n_det_opt.dat ${DIR2}/${FILE2}_energy_opt.dat > ${DIR2}/${FILE2}_result_opt.dat
-
+# File containing the results
 paste ${DIR2}/${FILE2}_n_det.dat ${DIR2}/${FILE2}_energy.dat > ${DIR2}/${FILE2}_tmp1.dat
 paste ${DIR2}/${FILE2}_tmp1.dat ${DIR2}/${FILE2}_pt2.dat > ${DIR2}/${FILE2}_tmp2.dat
-paste ${DIR2}/${FILE2}_tmp2.dat ${DIR2}/${FILE2}_rpt2.dat > ${DIR2}/${FILE2}_result.dat
+paste ${DIR2}/${FILE2}_tmp2.dat ${DIR2}/${FILE2}_rpt2.dat > ${DIR2}/${FILE2}_tmp3.dat
+paste ${DIR2}/${FILE2}_tmp3.dat ${DIR2}/${FILE2}_e_p_pt2.dat > ${DIR2}/${FILE2}_tmp4.dat
+paste ${DIR2}/${FILE2}_tmp4.dat ${DIR2}/${FILE2}_e_p_rpt2.dat > ${DIR2}/${FILE2}_result_fci.dat
 
 #       -p xeonv1 -N 1 -n 1 -c 16 --exclusive
 #       -p xeonv2 -N 1 -n 1 -c 20 --exclusive
@@ -106,4 +159,3 @@ paste ${DIR2}/${FILE2}_tmp2.dat ${DIR2}/${FILE2}_rpt2.dat > ${DIR2}/${FILE2}_res
 #       -p xeonv4 -N 1 -n 1 -c 28 --exclusive
 #       -p xeonv5 -N 1 -n 1 -c 32 --exclusive
 #       -p xeonv6 -N 1 -n 1 -c 32 --exclusive
-

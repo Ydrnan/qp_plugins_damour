b1f0cc5e2bd07013d1ce56e1d48663e63cdb95d1
ydamour <yann.damour@hotmail.fr>
2022-01-29 22:03:58 +0100

    save work 2

--- damour_tools/plot.py
+++ damour_tools/plot.py
@@ -24,8 +24,11 @@ def import_data(filename):
     return data
 
 def my_plot(ax, data, kind, **kwargs):
+    w_logx = False
+    w_logy = False
+
     # states
-    n_states = kwargs.get('n_states', 1)
+    n_states = int(kwargs.get('n_states', 1))
     mo = kwargs.get('mo', '')
     states=[]
     for i in range(n_states):
@@ -52,27 +55,32 @@ def my_plot(ax, data, kind, **kwargs):
         x_label = 'Number of determinants'
         y_label = 'Variational energy (E$_h$)'
         n_plots = n_states
+        w_logx = True
        
     elif (kind == 'e=f(pt2)'):
-        x = data[2]
+        x = []
         y = []
         p_label = []
         for i in range(n_states):
-            y.append(data[states[i]*5+2])
+            x.append(data[states[i]*5+2])
+            y.append(data[states[i]*5+1])
             p_label.append(mo+"s st "+str(states[i]))
         x_label = 'PT2 energy (E$_h$)'
         y_label = 'Variational energy (E$_h$)'
         n_plots = n_states
+
     elif (kind == 'e=f(rpt2)'):
-        x = data[2]
+        x = []
         y = []
         p_label = []
         for i in range(n_states):
-            y.append(data[states[i]*5+4])
+            x.append(data[states[i]*5+4])
+            y.append(data[states[i]*5+1])
             p_label.append(mo+"s st "+str(states[i]))
-        x_label = 'rPT2 energy E$_h$'
+        x_label = 'rPT2 energy (E$_h)$'
         y_label = 'Variational energy (E$_h$)'
         n_plots = n_states
+
     elif (kind == 'exc=f(ndet)'):
         x = data[0]
         y = []
@@ -87,81 +95,145 @@ def my_plot(ax, data, kind, **kwargs):
         x_label = 'Number of determinants'
         y_label = 'Excitation energy $\Delta_{E+PT2}$ (E$_h)$'
         n_plots = n_states-1
+        w_logx = True
     else:
         print("Unknown kind of plot"+kind+" read the fuc*ing doc")
         exit()
 
-    # Font-
-    #matplotlib.font_manager.findSystemFonts(fontpaths=None, fontext='ttf')
+    # Font
     #rc('font',**{'family':'sans-serif','sans-serif':['Helvetica']})
-    rc('font',**{'family':'serif','serif':['Times']})
-    rc('text', usetex=True)
-    
-    # output
-    fname = 'toto.png'
-    
-    # E = f(Ndet)
-    #fig, ax = plt.subplots(figsize=(9, 6), constrained_layout=False)
+    #rc('font',**{'family':'serif','serif':['Times']})
+    #rc('text', usetex=True)
     for i in range(n_plots):
-        ax.plot(x, y[i], label=p_label[i], marker='x', linewidth=2, markersize=6)
+        if (kind == 'e=f(pt2)') or (kind == 'e=f(rpt2)'):
+            ax.plot(x[i], y[i], label=p_label[i], marker='x', linewidth=2, markersize=6)
+        else:
+            ax.plot(x, y[i], label=p_label[i], marker='x', linewidth=2, markersize=6)
 
-    ax.legend(loc='best', fontsize=10)
     ax.set_xlabel(x_label, fontsize=20)#, fontname='Times New Roman')
     #ax.set_xlim(left=0.1, right=2)
     ax.tick_params(axis='both',which='major',  labelsize=20, length=4, width=1)
     #ax.set_xticks(np.arange(0, 2.1, step=0.2))
     ax.set_ylabel(y_label, fontsize=20)#, fontname='Times New Roman')
     #ax.set_ylim(bottom=0.1, top=5.)
-    #ax.set_yscale('log')
-    ax.set_xscale('log')
-    ax.minorticks_on()
-    ax.set_title("A beautiful title", fontsize=30)#, fontname='Times New Roman')
-    ax.grid(visible=True, which='major', axis='both', linewidth=0.5, linestyle='dashed')
-    #    plt.subplots_adjust(left  = 0.16,  # The position of the left edge of the subplots, as a fraction of the figure width
-    #        right = 0.98,   # The position of the right edge of the subplots, as a fraction of the figure width
-    #        bottom = 0.125,  # The position of the bottom edge of the subplots, as a fraction of the figure height
-    #        top = 0.9,      # The position of the top edge of the subplots, as a fraction of the figure height
-    #        wspace = None,   # The width of the padding between subplots, as a fraction of the average Axes width
-    #        hspace = None)   # The height of the padding between subplots, as a fraction of the average Axes width)
-    #    plt.savefig(fname, dpi='figure', format='png', metadata=None,
-    #            bbox_inches=None, pad_inches=0.1,
-    #            backend=None)
-    #    plt.show()
-    #
-    #data = import_data(infile)
-    #
-    #out = my_plot(data,kind='exc=f(ndet)', n_states=2, mo='OO')
+    if (w_logx): 
+        ax.set_xscale('log')
+    if (w_logy):
+        ax.set_yscale('log')
+    
     return ax
 
 import sys
+
+# test
+import getopt
+
+# init/ default
+n_states = 1
+basename = "plot"
+title = None
+grid = True
+mticks = True
+
+# read the command line arguments 
+try:
+    opts, args = getopt.getopt(sys.argv[1:],"hs:t:o:",["help","n_states=","title=","output=","grid=","mticks="])
+except getopt.GetoptError:
+    print("plot.py -h")
+    exit()
+
+for opt, arg in opts:
+    if opt == "-h":
+        print(" plot.py [options] <files>")
+        print("         -s <Number of states>   , default=", n_states)
+        print("         -t <Title>              , default=", title)
+        print("         -o <Output name>        , default=", basename)
+        print("         --grid=<Boolean>        , default=", grid)
+        print("         --mticks=<Boolean>      , default=", mticks)
+        sys.exit()
+    #elif opt in ("-x", "--mol"):
+    #    mol = arg
+    #elif opt in ("-b", "--basis"):
+    #    basis = arg
+    elif opt in ("-s", "--n_states"):
+        n_states = arg
+        if isinstance(n_states,int):
+            print("The number of states must be an integer")
+            sys.exit()
+            if n_states < 1:
+                print("The number of states must be > 1")
+                sys.exit()
+    elif opt in ("-t", "--title"):
+        title = arg
+    elif opt in ("-o", "--output"):
+        basename = arg
+    elif opt in ("--grid"):
+        grid = arg
+        print(grid)
+    elif opt in ("--mticks"):
+        mticks = arg
 
 # files
 list_files = []
 
 for arg in sys.argv:
     list_files.append(arg)
-list_files.pop(0)
+list_files.pop(0) # 1st time to remove the name of the prog
+
+# remove options in list_files
+i = 0
+l = len(list_files)
+while i < l:
+    if list_files[i][:2] == '--':
+        list_files.pop(i)
+        l = l-1
+    elif list_files[i][:1] == '-':
+        list_files.pop(i)
+        list_files.pop(i)
+        l = l-2
+    else:
+        i = i+1
+    print(i,l)
 
 print(list_files)
 
-fname = 'test.png'
-fig, ax = plt.subplots(figsize=(9, 6), constrained_layout=False)
+list_methods = ['e=f(ndet)', 'e=f(pt2)', 'e=f(rpt2)', 'exc=f(ndet)']
 
-for infile in list_files:
-    print(infile)
-    data = import_data(infile)
-    ax = my_plot(ax, data, kind='e=f(ndet)', n_states=1, st_0=0, mo='OO')
-    #ax = my_plot(ax, data, kind='e=f(ndet)', n_states=1, st_0=1, mo='NO')
+for method in list_methods:
+    figname = basename+'_'+method+'.png'
+    print(figname)
 
-plt.subplots_adjust(left  = 0.16,  # The position of the left edge of the subplots, as a fraction of the figure width
-    right = 0.98,   # The position of the right edge of the subplots, as a fraction of the figure width
-    bottom = 0.125,  # The position of the bottom edge of the subplots, as a fraction of the figure height
-    top = 0.9,      # The position of the top edge of the subplots, as a fraction of the figure height
-    wspace = None,   # The width of the padding between subplots, as a fraction of the average Axes width
-    hspace = None)   # The height of the padding between subplots, as a fraction of the average Axes width)
-plt.savefig(fname, dpi='figure', format='png', metadata=None,
-        bbox_inches=None, pad_inches=0.1,
-        backend=None)
-plt.show()
+    # Font
+    #rc('font',**{'family':'sans-serif','sans-serif':['Helvetica']})
+    rc('font',**{'family':'serif','serif':['Times']})
+    rc('text', usetex=True)
+
+    fig, ax = plt.subplots(figsize=(9, 6), constrained_layout=False)
+
+    for infile in list_files:
+        print(infile)
+        data = import_data(infile)
+        print(n_states)
+        print(grid)
+        ax = my_plot(ax, data, kind=method, n_states=n_states, st_0=0, mo='OO', title=title)
+ 
+    ax.legend(loc='best', fontsize=10)
+    if (mticks):
+        ax.minorticks_on()
+    if (title != None):
+        ax.set_title(title, fontsize=30)#, fontname='Times New Roman')
+    if (grid):
+        ax.grid(visible=True, which='major', axis='both', linewidth=0.5, linestyle='dashed')
+    
+    plt.subplots_adjust(left  = 0.16,  # The position of the left edge of the subplots, as a fraction of the figure width
+        right = 0.98,   # The position of the right edge of the subplots, as a fraction of the figure width
+        bottom = 0.125,  # The position of the bottom edge of the subplots, as a fraction of the figure height
+        top = 0.9,      # The position of the top edge of the subplots, as a fraction of the figure height
+        wspace = None,   # The width of the padding between subplots, as a fraction of the average Axes width
+        hspace = None)   # The height of the padding between subplots, as a fraction of the average Axes width)
+    plt.savefig(figname, dpi='figure', format='png', metadata=None,
+            bbox_inches=None, pad_inches=0.1,
+            backend=None)
+    plt.show()
 
 

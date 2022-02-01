a83a7765fb5e8faecf42da30452cccd38dd105c0
ydamour <yann.damour@hotmail.fr>
2022-02-01 22:29:58 +0100

    numpy for extrapolation_fci

--- damour_tools/extrapolation_fci.py
+++ damour_tools/extrapolation_fci.py
@@ -2,57 +2,57 @@
 import sys
 from linear_regression import *
 from convert_units import *
+from import_data import *
 
 def extrapolation_fci(filename, **kwargs):
-    f = open(filename,'r')
-    firstline = f.readline() # just some comments one the first line
-    lenght = len(firstline.split())
-    n_states = lenght//5
-    print("\n%17s %2d\n"%("Number of states:",n_states))
-    lines = f.readlines()
-    f.close
+    print("Filename:",filename)
+    data = import_data(filename)
     
-    if (len(lines) < 3):
-        print("Need at least 3 points, found "+str(len(lines))+", exit")
+    n_states = np.size(data,0) // 5
+    print("\nNumber of states:", n_states)
+
+    n_data = np.size(data,1)
+
+    if (n_data < 3):
+        print("\nNeed at least 3 points, found "+str(n_data)+", exit")
         sys.exit()
-    
+
     print("f(PT2) = a*PT2 + b\n")
     
     weight_type = kwargs.get('weight', 'None')
     print("Weight of the points for the linear regression:", weight_type)
 
-    ref = [] # Extrapolated energies of the ground state for diff nb of points
-    for state in range(0,n_states):
-        E = []
-        PT2 = []
-        weight = []
-        for line in lines:
-            l = line.split()
-            E.append(float(l[1+5*state]))
-            PT2.append(float(l[2+5*state]))
+    a = np.zeros((n_data, n_states))
+    b = np.zeros((n_data, n_states))
+    R2 = np.zeros((n_data, n_states))
+    exc = np.zeros((n_data, n_states))
 
+    # Extrapolated energies of the ground state for diff nb of points
+    for state in range(0,n_states):
+        E = data[1+5*state]
+        PT2 = data[2+5*state]
+        
+        if weight_type == 'pt2': 
+            weight = 1.0/PT2
+        elif weight_type == 'pt2**2':
+            weight = 1.0/PT2**2
+        else:
+            weight = np.full(len(E), 1.0)
+    
         print("%s %2d" %("\nState:",state))
         if (state == 0):
             print("%3s %6s %13s %9s"%("n","a","b (Ha)","R^2"))
         else:
             print("%3s %6s %13s %12s %10s %7s"%("n","a","b (Ha)","ref (Ha)","Exc (eV)","R^2"))
     
-        i = 0
-        for nb_points in range(3,min(8,len(lines))+1):
-            for w in range(len(PT2)):
-                if weight_type == 'pt2': 
-                    weight.append(1.0/PT2[w])
-                elif weight_type == 'pt2**2':
-                    weight.append(1.0/PT2[w]**2)
-                else:
-                    weight.append(1.0)
-    
+        for nb_points in range(3,min(8,n_data)+1):
+            i = nb_points-3
             # f(x) = a * x + b
             # with weights
             reg = lin_reg_v2(PT2,E,weight,nb_points)
-            a = reg[0] 
-            b = reg[1]
-            R2 = reg[2]
+            a[i][state] = reg[0] 
+            b[i][state] = reg[1]
+            R2[i][state] = reg[2]
     
             # without
             #reg = lin_reg(PT2,E,nb_points)
@@ -61,13 +61,14 @@ def extrapolation_fci(filename, **kwargs):
             #R2=reg[2]
     
             if (state == 0):
-                ref.append(b)
-                print("| %1d | %6.4f | %8.4f | %8.6f |"%( nb_points, a, b, R2))
+                print("| %1d | %6.4f | %8.4f | %8.6f |"%( nb_points, a[i][state], b[i][state], R2[i][state]))
             else:
-                exc = b - ref[i]
-                exc = Ha_to_eV(exc)
-                print("| %1d | %6.4f | %8.4f | %8.4f | %7.4f | %8.6f |"%( nb_points, a, b, ref[i], exc ,R2))
-                i = i +1
+                exc[i][state] = Ha_to_eV(b[i][state] - b[i][0])
+                print("| %1d | %6.4f | %8.4f | %8.4f | %7.4f | %8.6f |"%( nb_points, a[i][state],
+                      b[i][state], b[i][0], exc[i][state] ,R2[i][state]))
+
+    # Energy with 4 points
+    return n_data,a,b,R2,exc
 
 # main for extrapolation
 import getopt
--- damour_tools/plot.py
+++ damour_tools/plot.py
@@ -12,27 +12,10 @@ import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib import rc
 from convert_units import *
+from import_data import *
 
 def parse_boolean(s):
     return s == 'True'
-
-def import_data(filename, **kwargs):
-    last = int(kwargs.get('last', 0))
-    with open(filename,'r') as f:
-        trash = f.readline() # comments line
-        lines = f.readlines()
-    
-    data = []
-    for line in lines:
-        data.append(line.split())
-    data = np.array(data)
-    if (last != 0):
-        n=len(data)
-        data=data[n-last:][:]
-    data = data.T
-    data = np.asarray(data, dtype=float, order='F') # str to float
-    
-    return data
 
 def my_plot(ax, data, kind, **kwargs):
     w_logx = False

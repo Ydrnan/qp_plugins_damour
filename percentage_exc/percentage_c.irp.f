ea66d068ccb5ebb47142e29fd4b20ba3b8f53e1d
ydamour <yann.damour@hotmail.fr>
2022-05-21 08:57:56 +0200

    update org mode

--- percentage_exc/percentage_c.irp.f
+++ percentage_exc/percentage_c.irp.f
@@ -1,3 +1,17 @@
+
+
+! Provided
+! | max_exc_degree    | integer | maximum excitation degree        |
+! | percentage_in_exp | logical | to use or not exponential format |
+
+! internal
+! | percentage(max_exc_degree+1,N_states) | double precision | The ith element is the percentage of |
+! |                                       |                  | the i-1 coefficients/excitations     |
+! | list_states(N_states)                 | integer          | list of states                       |
+! | accu(N_states)                        | double precision | temporary array                      |
+! | exc                                   | character        | excitation degree                    |
+
+
 subroutine run_print_percentage_c
 
   implicit none
@@ -95,83 +109,62 @@ subroutine run_print_percentage_c
 
 end
 
+! Calculation of the percentages
+
+! \begin{equation}
+! C_i = \sum_i c_i^2
+! \end{equation}
+
+! Provided
+! | N_states                 | integer            | number of states          |
+! | max_exc_degree           | integer            | maximum excitation degree |
+! | HF_bitmask               | integer(bitstring) | HF determinants           |
+! | psi_det(N_int,2,N_det)   | integer(bitstring) | Determinants              |
+! | psi_coef(N_det,N_states) | double precision   | CI coefficients           |
+! | N_int                    | integer            | Number of int per det     |
+! | N_det                    | integer            | Number of det             |
+
+! Out
+! | percentage(max_exc_degree + 1, n_states) | double precision | The ith element is the percentage of |
+! |                                          |                  | the i-1 coefficients/excitations     |
+
+! Internal
+! | exc_degree | integer | excitation degree              |
+! | idx_hf     | integer | index of the HF det in psi_det |
+! | i,s        | integer | dummy indexes                  |
+
+
 subroutine percentage_c(percentage)
 
   implicit none
 
-  ! max_exc_degree = min(2*mo_num - elec_alpha_num - elec_beta_num, elec_alpha_num + elec_beta_num)
-  ! + 1 to have the %C_0
   ! out
-  double precision, intent(out) :: percentage(max_exc_degree + 1, n_states) 
+  double precision, intent(out) :: percentage(max_exc_degree + 1, N_states) 
 
   ! internal
-  integer :: i, s, exc_degree, idx_hf
+  integer :: i, s, degree, idx_hf
 
   percentage = 0d0
 
   ! %C(n,s_state) = \sum_i psi_coef(i,s)**2 s.t. excitation_degree(|HF>,|i>) = n
 
   ! Contribution of HF det
-  call find_hf_in_wf(psi_det,N_det,N_int,idx_hf)
-  do s = 1, n_states
+  call find_hf(psi_det,N_det,N_int,idx_hf)
+  do s = 1, N_states
     percentage(1,s) = psi_coef(idx_hf,s)**2
   enddo
   
   ! Others determinants
-  do i = 1, n_det
-    call get_excitation_degree(HF_bitmask, psi_det(1,1,i), exc_degree, n_int)
-    if (exc_degree == 0) then
+  do i = 1, N_det
+    call get_excitation_degree(HF_bitmask, psi_det(1,1,i), degree, n_int)
+    if (degree == 0) then
       cycle
     endif
-    do s = 1, n_states
-      percentage(exc_degree+1, s) = percentage(exc_degree+1, s) + psi_coef(i,s)**2
+    do s = 1, N_states
+      percentage(degree+1, s) = percentage(degree+1, s) + psi_coef(i,s)**2
     enddo
   enddo
 
   percentage = percentage *100d0
-
-end
-
-subroutine find_hf_in_wf(psidet,Ndet,Nint,idx_hf)
-
-  implicit none
-
-  ! in
-  integer(bit_kind), intent(in) :: psidet(Nint,2,Ndet) 
-  integer, intent(in) :: Ndet                          
-  integer, intent(in) :: Nint                          
-
-  ! out
-  integer :: idx_hf
-
-  ! internal
-  integer :: i_int,j
-  logical :: is_hf
-
-  idx_hf = 0
-  ! %T_0 
-  do j = 1, Ndet
-    is_hf = .True.
-    do i_int = 1, Nint
-      if (psidet(i_int,1,j) /= HF_bitmask(i_int,1) .or. &
-          psidet(i_int,2,j) /= HF_bitmask(i_int,2)) then
-        is_hf = .False.
-        exit
-      endif
-      if (.not. is_hf) then
-        exit
-      endif
-    enddo
-    if (.not. is_hf) then
-      cycle
-    endif
-    idx_hf = j
-    exit
-  enddo
-
-  if (idx_hf == 0) then
-    print*,'Hf determinant not found, abort'
-    call abort
-  endif
 
 end
